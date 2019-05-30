9ac569666ca0012ecb95d5f839a71f7adb4ba873
Greg Wiley <gwiley@cj.com>
2019-05-30 09:27:49 -0700

    put to s3

--- src/submit.rs
+++ src/submit.rs
@@ -1,5 +1,6 @@
-use crate::effect::repo::*;
-use crate::effect::file::*;
+use crate::effect::file::{FileSystem};
+use crate::effect::repo::{Git};
+use crate::effect::s3::{S3, S3Types};
 use crate::effect::zip::{Zip, ZipTypes};
 
 pub trait SubmitTypes {
@@ -12,17 +13,19 @@ pub trait Submit: SubmitTypes {
 
 impl<T> Submit for T
     where
-        T: Git + FileSystem + SubmitTypes + Zip,
+        T: Git + FileSystem + SubmitTypes + Zip + S3,
         <T as SubmitTypes>::Error:
-            From<<T as Git>::Error> +
             From<<T as FileSystem>::Error> +
+            From<<T as Git>::Error> +
+            From<<T as S3Types>::Error> +
             From<<T as ZipTypes>::Error>
         {
-    fn submit_to_pipeline(&self, repo_url: &str, _s3_bucket: &str, _s3_key: &str)  -> Result<(), Self::Error> {
+    fn submit_to_pipeline(&self, repo_url: &str, s3_bucket: &str, s3_key: &str)  -> Result<(), Self::Error> {
         let path = self.mk_temp_dir()?;
         let archive = self.mk_temp_file()?;
         self.clone_repo(repo_url, &path )?;
         self.zip_directory(&path, &archive)?;
+        self.put_object(&archive, s3_bucket, s3_key)?;
         Ok(())
     }
 }
@@ -40,15 +43,18 @@ mod tests {
         let tmpdir = "X29304";
         let tmpfile = "90a90AAC";
         let repo = "git@foo:thingbarnone";
+        let bucket = "sourcebucket";
+        let key = "sourceobjectname";
 
 
         let r = R2 {
             fs: (Some(PathBuf::from(tmpdir)), Some(PathBuf::from(tmpfile))),
             git: [(repo.to_owned(), tmpdir.to_owned())].iter().cloned().collect(),
             zip: [(tmpdir.to_owned(), tmpfile.to_owned())].iter().cloned().collect(),
+            s3: [(tmpfile.to_owned(), bucket.to_owned(), key.to_owned())].iter().cloned().collect(),
         };
 
-        let actual = r.submit_to_pipeline(repo, "", "");
+        let actual = r.submit_to_pipeline(repo, bucket, key);
 
         assert_eq!(actual, Ok(()));
     }
@@ -58,15 +64,18 @@ mod tests {
         let tmpdir = "X29304";
         let tmpfile = "90a90AAC";
         let repo = "git@foo:thingbarnone";
+        let bucket = "sourcebucket";
+        let key = "sourceobjectname";
 
 
         let r = R2 {
             fs: (Some(PathBuf::from(tmpdir)), Some(PathBuf::from(tmpfile))),
             git: [(repo.to_owned(), tmpdir.to_owned())].iter().cloned().collect(),
-            zip: HashSet::new(),
+            zip: ZIP::default(),
+            s3: [(tmpfile.to_owned(), bucket.to_owned(), key.to_owned())].iter().cloned().collect(),
         };
 
-        let actual = r.submit_to_pipeline(repo, "", "");
+        let actual = r.submit_to_pipeline(repo, bucket, key);
 
         assert_eq!(actual, Err(()));
     }
@@ -76,15 +85,18 @@ mod tests {
         let tmpdir = "X29304";
         let tmpfile = "90a90AAC";
         let repo = "git@foo:thingbarnone";
+        let bucket = "sourcebucket";
+        let key = "sourceobjectname";
 
 
         let r = R2 {
             fs: (Some(PathBuf::from(tmpdir)), Some(PathBuf::from(tmpfile))),
-            git: HashSet::new(),
+            git: GIT::default(),
             zip: [(tmpdir.to_owned(), tmpfile.to_owned())].iter().cloned().collect(),
+            s3: [(tmpfile.to_owned(), bucket.to_owned(), key.to_owned())].iter().cloned().collect(),
         };
 
-        let actual = r.submit_to_pipeline(repo, "", "");
+        let actual = r.submit_to_pipeline(repo, bucket, key);
 
         assert_eq!(actual, Err(()));
     }
@@ -94,15 +106,18 @@ mod tests {
         let tmpdir = "X29304";
         let tmpfile = "90a90AAC";
         let repo = "git@foo:thingbarnone";
+        let bucket = "sourcebucket";
+        let key = "sourceobjectname";
 
 
         let r = R2 {
             fs: (None, Some(PathBuf::from(tmpfile))),
-            git: HashSet::new(),
+            git: [(repo.to_owned(), tmpdir.to_owned())].iter().cloned().collect(),
             zip: [(tmpdir.to_owned(), tmpfile.to_owned())].iter().cloned().collect(),
+            s3: [(tmpfile.to_owned(), bucket.to_owned(), key.to_owned())].iter().cloned().collect(),
         };
 
-        let actual = r.submit_to_pipeline(repo, "", "");
+        let actual = r.submit_to_pipeline(repo, bucket, key);
 
         assert_eq!(actual, Err(()));
     }
@@ -112,18 +127,43 @@ mod tests {
         let tmpdir = "X29304";
         let tmpfile = "90a90AAC";
         let repo = "git@foo:thingbarnone";
+        let bucket = "sourcebucket";
+        let key = "sourceobjectname";
 
 
         let r = R2 {
             fs: (Some(PathBuf::from(tmpdir)), None),
-            git: HashSet::new(),
+            git: [(repo.to_owned(), tmpdir.to_owned())].iter().cloned().collect(),
             zip: [(tmpdir.to_owned(), tmpfile.to_owned())].iter().cloned().collect(),
+            s3: [(tmpfile.to_owned(), bucket.to_owned(), key.to_owned())].iter().cloned().collect(),
         };
 
-        let actual = r.submit_to_pipeline(repo, "", "");
+        let actual = r.submit_to_pipeline(repo, bucket, key);
 
         assert_eq!(actual, Err(()));
     }
+
+    #[test]
+    fn put_error() {
+        let tmpdir = "X29304";
+        let tmpfile = "90a90AAC";
+        let repo = "git@foo:thingbarnone";
+        let bucket = "sourcebucket";
+        let key = "sourceobjectname";
+
+
+        let r = R2 {
+            fs: (Some(PathBuf::from(tmpdir)), Some(PathBuf::from(tmpfile))),
+            git: [(repo.to_owned(), tmpdir.to_owned())].iter().cloned().collect(),
+            zip: [(tmpdir.to_owned(), tmpfile.to_owned())].iter().cloned().collect(),
+            s3: SSS::default(),
+        };
+
+        let actual = r.submit_to_pipeline(repo, bucket, key);
+
+        assert_eq!(actual, Err(()));
+    }
+
 
     type FS = (Option<PathBuf>, Option<PathBuf>);
     impl FileSystem for FS {
@@ -167,10 +207,23 @@ mod tests {
         }
     }
 
+    type SSS = HashSet<(String, String, String)>;
+    impl S3Types for SSS {type Error = ();}
+    impl S3 for SSS {
+        fn put_object(&self, file: &Path, bucket: &str, key: &str) -> Result<(), Self::Error> {
+            if self.contains(&(file.to_str().unwrap().to_owned(), bucket.to_owned(), key.to_owned())) {
+                Ok (())
+            } else {
+                Err(())
+            }
+        }
+    }
+
     struct R2 {
         fs: FS,
         git: GIT,
         zip: ZIP,
+        s3: SSS,
     }
 
     impl FileSystem for R2 {
@@ -191,7 +244,7 @@ mod tests {
     }
 
     impl ZipTypes for R2 {
-        type Error = <HashSet<(String, String)> as ZipTypes>::Error;
+        type Error = <ZIP as ZipTypes>::Error;
     }
 
     impl Zip for R2 {
@@ -200,6 +253,15 @@ mod tests {
         }
     }
 
+    impl S3Types for R2 {
+        type Error = <SSS as S3Types>::Error;
+    }
+
+    impl S3 for R2 {
+        fn put_object(&self, file: &Path, bucket: &str, key: &str) -> Result<(), Self::Error> {
+            self.s3.put_object(file, bucket, key)
+        }
+    }
 
     impl SubmitTypes for R2 {type Error = ();}
 
