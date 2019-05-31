1a59be24708371fff8aad3b700ff18e1ecb2b43e
Greg Wiley <gwiley@cj.com>
2019-05-31 13:03:55 -0700

    prototype zip to existing file

--- src/effect/zip.rs
+++ src/effect/zip.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{Read, Seek, Write};
+use std::io::{Read, Seek, Write, SeekFrom};
 use std::path::Path;
 use walkdir::{DirEntry, WalkDir};
 use zip::result::ZipError;
@@ -14,6 +14,9 @@ pub trait Zip: ZipTypes {
     fn zip_directory(&self, _from: &Path, _to: &Path) -> Result<(), Self::Error> {
         unimplemented!();
     }
+    fn zip_directory_f(&self, _from: &Path, _to: &mut File) -> Result<(), Self::Error> {
+        unimplemented!();
+    }
 }
 
 pub trait InIO {}
@@ -25,11 +28,17 @@ where
     <T as ZipTypes>::Error: From<std::io::Error>,
 {
     fn zip_directory(&self, dir: &Path, arch: &Path) -> Result<(), Self::Error> {
+        let mut arch = File::create(&arch)?;
+        self.zip_directory_f(dir, &mut arch)
+    }
+
+    fn zip_directory_f(&self, dir: &Path, arch: &mut File) -> Result<(), Self::Error> {
         if !dir.is_dir() {
             return Err(Self::Error::from(ZipError::FileNotFound));
         }
 
-        let arch = File::create(&arch)?;
+        arch.seek(SeekFrom::Start(0))?;
+        arch.set_len(0)?;
 
         let walk = WalkDir::new(dir).into_iter();
         zipd(
@@ -49,38 +58,40 @@ where
                 _ => None,
             }),
         )?;
+        arch.sync_data()?;
         Ok(())
     }
+
 }
 
-pub fn zip(dir: &Path, arch: &Path) -> Result<(), ZipError> {
-    if !dir.is_dir() {
-        return Err(ZipError::FileNotFound);
-    }
+// pub fn zip(dir: &Path, arch: &Path) -> Result<(), ZipError> {
+//     if !dir.is_dir() {
+//         return Err(ZipError::FileNotFound);
+//     }
 
-    let arch = File::create(&arch)?;
+//     let arch = File::create(&arch)?;
 
-    let walk = WalkDir::new(dir).into_iter();
-    zipd(
-        arch,
-        dir,
-        &mut walk.filter_map(|e| match e {
-            Ok(entry) => {
-                let path = entry.path();
-                let name = path.strip_prefix(Path::new(dir)).unwrap();
-                let name = name.to_str()?;
-                if name != ".git" && (name.len() < 5 || &name[0..5] != ".git/") {
-                    Some(entry)
-                } else {
-                    None
-                }
-            }
-            _ => None,
-        }),
-    )
-}
+//     let walk = WalkDir::new(dir).into_iter();
+//     zipd(
+//         arch,
+//         dir,
+//         &mut walk.filter_map(|e| match e {
+//             Ok(entry) => {
+//                 let path = entry.path();
+//                 let name = path.strip_prefix(Path::new(dir)).unwrap();
+//                 let name = name.to_str()?;
+//                 if name != ".git" && (name.len() < 5 || &name[0..5] != ".git/") {
+//                     Some(entry)
+//                 } else {
+//                     None
+//                 }
+//             }
+//             _ => None,
+//         }),
+//     )
+// }
 
-fn zipd<T>(writer: T, prefix: &Path, it: &mut Iterator<Item = DirEntry>) -> Result<(), ZipError>
+fn zipd<T>(writer: &mut T, prefix: &Path, it: &mut Iterator<Item = DirEntry>) -> Result<(), ZipError>
 where
     T: Write + Seek,
 {
